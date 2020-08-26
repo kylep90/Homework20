abd0709df269c925a80b20bebf418cad9396e138
Kyle Pingue <pinguek@gmail.com>
2020-08-25 23:45:05 -0500

    Updates

---
+++ precache-manifest.8860c950e3cacc01ca124bfcd6421fd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6424e6728d3e9b9e6c7711151ffc4633",
+    "url": "/Homework20/index.html"
+  },
+  {
+    "revision": "ec85e9a40cc0b082b463",
+    "url": "/Homework20/static/css/main.4976795d.chunk.css"
+  },
+  {
+    "revision": "1139de87b0d7edd4690d",
+    "url": "/Homework20/static/js/2.64773b41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Homework20/static/js/2.64773b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec85e9a40cc0b082b463",
+    "url": "/Homework20/static/js/main.d7cd2175.chunk.js"
+  },
+  {
+    "revision": "8434ed6f38cace6dc716",
+    "url": "/Homework20/static/js/runtime-main.f0c816f6.js"
+  },
+  {
+    "revision": "eff7861b42d8b88647afc84ed863657c",
+    "url": "/Homework20/static/media/Grenada.eff7861b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Homework20/static/media/logo.5d5d9eef.svg"
+  }
+]);
